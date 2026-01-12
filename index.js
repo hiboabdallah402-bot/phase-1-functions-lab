1524387f898eaff73e367fd44b83ecb94700f277
hibo <hiboabdallah402@gmail.com>
2026-01-12 09:25:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+// index.js
+
+const HQ_BLOCK = 42;
+const FEET_PER_BLOCK = 264;
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - HQ_BLOCK);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * FEET_PER_BLOCK;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * FEET_PER_BLOCK;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance > 2500) {
+    return 'cannot travel that far';
+  }
+
+  if (distance <= 400) {
+    return 0;
+  }
+
+  if (distance > 2000) {
+    return 25;
+  }
+
+  return (distance - 400) * 0.02;
+}
+
